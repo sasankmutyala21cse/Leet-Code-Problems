f49894fd2e0af43172e3670bd96df7f9626b788e
Mutyala Sasank <sasankmutyala21@gmail.com>
2026-09-14 20:59:49 +0530

    Time: 13 ms (88.21%) | Memory: 12.5 MB (2.14%) - LeetSync

---
+++ 1260-day-of-the-year/day-of-the-year.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def dayOfYear(self, date):
+        """
+        :type date: str
+        :rtype: int
+        """
+        y=int(date[0:4])
+        r=0
+        d=int(date[8:])
+        m=int(date[5:7])
+        if m==1:
+            r+=d
+        if m==2:
+            r=31+d
+        if m==3:
+            r=59+d
+        if m==4:
+            r=90+d
+        if m==5:
+            r=120+d
+        if m==6:
+            r=151+d
+        if m==7:
+            r=181+d
+        if m==8:
+            r=212+d
+        if m==9:
+            r=243+d
+        if m==10:
+            r=273+d
+        if m==11:
+            r=304+d
+        if m==12:
+            r=334+d
+        if (y % 400 == 0) or (y % 4 == 0 and y % 100 != 0):
+            if m>=3:
+                r+=1
+        return r
+        
