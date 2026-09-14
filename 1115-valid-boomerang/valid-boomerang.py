aa54835ed445ed8cf393ec0558b2d123ade62a3f
Mutyala Sasank <sasankmutyala21@gmail.com>
2026-09-14 19:40:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (13.16%) - LeetSync

---
+++ 1115-valid-boomerang/valid-boomerang.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isBoomerang(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: bool
+        """
+        x1,y1=points[0]
+        x2,y2=points[1]
+        x3,y3=points[2]
+        return (x2-x1)*(y3-y1)!=(y2-y1)*(x3-x1)
+        
